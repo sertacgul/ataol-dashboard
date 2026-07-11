6c76d96ea2126df4ffe74be129bc614ea9e559a9
sertacgul <81302385+sertacgul@users.noreply.github.com>
2026-07-11 22:31:12 +0300

    fix: remove unused useRef import from EmailFinder

--- actledger/src/pages/email-finder/EmailFinder.jsx
+++ actledger/src/pages/email-finder/EmailFinder.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { api } from '../../lib/api'
 import { useT } from '../../contexts/LanguageContext'
 
